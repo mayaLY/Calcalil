2837572c908f16df512128d3c202e14bae7b6ae1
MayaLY <maya_levy@yahoo.com>
2025-07-25 08:25:45 +0300

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,33 @@
-console.log("Hello, world!");
+import express from 'express';
+const app = express();
+const port = 3000;
+import cookieParser from 'cookie-parser';
+
+//connection to db
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://mayalevy20:e0ok6g2sVajOLOhu@cluster0.emeus.mongodb.net/calcalil').then(()=>{
+  console.log('connected to db')
+})
+.catch((err:any)=>{
+  console.log(err)
+});
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+app.use(express.static('public'));
+
+
+
+app.get('/', (req:any, res:any) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
