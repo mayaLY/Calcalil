ef0cea354df003ba3c521e76fecba0f9e36c6524
HotNuggets <mishamandark@gmail.com>
2025-08-06 16:28:42 +0300

    update

---
+++ client/Calcalil/src/view/pages/LoanReturnCalculatorPage/LoanCalculatorPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+const LoanCalculatorPage = () => {
+const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div>LoanReturnCalculatorPage</div>
+  )
+}
+
+export default LoanCalculatorPage
--- client/Calcalil/src/view/pages/LoanReturnCalculatorPage/LoanReturnCalculatorPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LoanReturnCalculatorPage = () => {
-  return (
-    <div>LoanReturnCalculatorPage</div>
-  )
-}
-
-export default LoanReturnCalculatorPage
