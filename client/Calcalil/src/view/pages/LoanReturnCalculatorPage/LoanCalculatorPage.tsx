42e141fb75ba87af10920ff3a6057fed2015ac4d
HotNuggets <mishamandark@gmail.com>
2025-08-07 15:53:16 +0300

    update

--- client/Calcalil/src/view/pages/LoanReturnCalculatorPage/LoanCalculatorPage.tsx
+++ client/Calcalil/src/view/pages/LoanReturnCalculatorPage/LoanCalculatorPage.tsx
@@ -3,9 +3,26 @@ import { useState } from 'react'
 
 
 const LoanCalculatorPage = () => {
-const [isOpen, setIsOpen] = useState(false)
+const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>LoanReturnCalculatorPage</div>
+    <div id="calculator">LoanReturnCalculatorPage
+      <div id="parameters">
+        <div id="monthOrSum">
+        <span>לחשב את:</span>
+        <button>החזר חודשי</button>
+        <button>סכום ההלוואה</button>
+        </div>
+        <div id="method">
+          <span>אופן החזר הלוואה</span>
+          <button>חזר קבוע (לוח שפיצר)</button>
+          <button>החזר קרן שווה</button>
+        </div>
+      
+      </div>
+      <div id="form-window"></div>
+    
+    
+    </div>
   )
 }
 
